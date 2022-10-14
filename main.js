c3cc52a2a4690071a5458255e5226d6c0b1c56f5
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-14 10:30:02 -0400

    update

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+// let isPrime = function(num){
+//     if (num < 2){
+//         return false;
+//     }
+
+//     for(let i = 2; i < num; i++){
+//         if(num % i === 0){
+//             return false
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(isPrime(5))
+// console.log(isPrime(12));
+
+// let kids = ["Jace", "Jazz", "Jada", "Jack"]
+
+
+// // function output(item){
+// //     let upper = item.toUpper
+// //     console.log(upper)
+// // }
+
+// // console.log(upperk)
+
+// kids.forEach(function(item, index, array){
+//     if(item === "Jace"){
+//         item = item.toUpperCase()
+//     } else {
+//         item = item.toLowerCase();
+//     }
+
+//     console.log(index, item)
+// })
+
+
+// let names = ["Jace","Jazz",  "Jada", "Jax"]
+// names.forEach(name => {
+//     console.log(name.toUpperCase())
+// })
+
+
+// let numbers = [6,9,4]
+// numbers.forEach(num => {
+//     let array = [];
+//     array.push(num * 2)
+//     console.log(array);
+// })
